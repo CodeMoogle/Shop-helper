d7b73554f95b7b3d533f34eb80ce942867057418
CodeMoogle <codemoogle90@gmail.com>
2020-10-29 21:42:27 +0300

    Login view added, base and ustils styles updated

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,14 @@
 <template>
 	<div id="app">
-		<h1>Shop-helper</h1>
-		<router-view />
+		<!-- header -->
+		<section class="app__content">
+			<router-view />
+		</section>
 	</div>
 </template>
 
 <style lang="scss">
 	body {
 		background-color: var(--primary-color);
-		& h1 {
-			color: var(--info-color);
-		}
 	}
 </style>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="login container flex-column">
+		<h1>Login</h1>
+		<form class="form" @submit.prevent="loginHandler">
+			<div class="form-group">
+				<input
+					type="email"
+					name="email"
+					id="email"
+					class="form-control"
+					placeholder="example@gmail.com"
+					required
+					v-model="email"
+				/>
+				<label for="email" class="form-label">Email</label>
+			</div>
+
+			<div class="form-group">
+				<input
+					type="password"
+					name="password"
+					id="password"
+					class="form-control"
+					placeholder="Password"
+					required
+					v-model="password"
+				/>
+				<label for="password" class="form-label">Password</label>
+			</div>
+
+			<input type="submit" value="Login" class="btn" />
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Login",
+	data() {
+		return {
+			email: "",
+			password: "",
+		};
+	},
+	methods: {
+		loginHandler() {
+			console.log("login");
+			this.email = "";
+			this.password = "";
+		},
+	},
+};
+</script>
+
+<style lang="scss">
+	.login {
+		& h1 {
+			font-size: 40px;
+			margin-bottom: 40px;
+		}
+		& .btn {
+			display: block;
+			margin: 0 auto;
+			text-transform: uppercase;
+			padding: 1em 3em;
+		}
+	}
+</style>
